6addd49ef29c9972690c479205ea12dd085e474c
Johannes Königer <koeniger@univention.de>
2022-05-30 11:54:45 +0200

    Bug #54793: New test for school name validation

---
+++ ucs-school-lib/modules/ucsschool/lib/tests/test_validation_school.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+def _inside_docker():
+    try:
+        import ucsschool.kelvin.constants
+    except ImportError:
+        return False
+    return ucsschool.kelvin.constants.CN_ADMIN_PASSWORD_FILE.exists()
+
+
+pytestmark = pytest.mark.skipif(
+    not _inside_docker(),
+    reason="Must run inside Docker container started by appcenter.",
+)
+
+
+@pytest.mark.asyncio
+async def test_schoolname_validation(create_ou_using_python, udm_kwargs, ldap_base):
+    """Test if an invalid school ou name is raising the expected exception
+
+    - Related Bug: #54793
+    """
+
+    with pytest.raises(ValueError, match="'Invalid school name'"):
+        ou_name_kelvin = await create_ou_using_python(ou_name="ba€d_ou_name", cache=False)
