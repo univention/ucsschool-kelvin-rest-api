e5e2cd23c5aa5b778228d881cc4c3e8014b887ed
Carlos García-Mauriño <garcia-maurino.extern@univention.de>
2022-06-20 13:56:46 +0300

    fixup! Bug #54812: change userExpiry syntax to date2

--- univention-directory-manager-modules-slim/univention/admin/syntax.py
+++ univention-directory-manager-modules-slim/univention/admin/syntax.py
@@ -416,8 +416,7 @@ class date2(date):  # fixes the century
     >>> date2.parse('just a string') # doctest: +IGNORE_EXCEPTION_DETAIL
     Traceback (most recent call last):
     ...
-    valueError: Not a valid Date. Year must be between 1961 and 2099, month
-                between 1 and 12 and day between 1 and 31.
+    valueError: Not a valid Date
     """
 
     @classmethod
@@ -435,12 +434,7 @@ class date2(date):  # fixes the century
                 if year >= 70:  # Epoch 0
                     return "19%02d-%02d-%02d" % (year, month, day)
                 return "20%02d-%02d-%02d" % (year, month, day)
-        raise valueError(
-            _(
-                "Not a valid Date. Year must be between 1961 and 2099, month "
-                "between 1 and 12 and day between 1 and 31."
-            )
-        )
+        raise valueError(_("Not a valid Date"))
 
     @classmethod
     def to_datetime(cls, value):
