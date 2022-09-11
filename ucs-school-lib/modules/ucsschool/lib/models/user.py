6e40d47bb1e217cb1b7713730d15b24071429fc1
Daniel Troeder <troeder@univention.de>
2022-09-12 01:53:49 +0200

    Bug #55103: linting fixes

--- ucs-school-lib/modules/ucsschool/lib/models/user.py
+++ ucs-school-lib/modules/ucsschool/lib/models/user.py
@@ -226,7 +226,9 @@ class User(RoleSupportMixin, UCSSchoolHelperAbstractClass):
         if not obj:
             raise noObject("Could not read %r" % (self.dn,))
         if "ucsschoolSchool" in obj.oldattr:  # TODO: HTTP UdmObject does not have "oldattr"!
-            return object_class.encode("UTF-8") in obj.oldattr.get("objectClass", [])  # TODO: see line above
+            return object_class.encode("UTF-8") in obj.oldattr.get(
+                "objectClass", []
+            )  # TODO: see line above
         return fallback(self.school, self.dn)
 
     @classmethod
