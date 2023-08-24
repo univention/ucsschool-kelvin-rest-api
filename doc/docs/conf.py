6a4002b0c2e83d1b0d2cd74b1641feb3b1c1cfe7
Tobias Wenzel <wenzel@univention.de>
2023-08-24 11:51:39 +0200

    Issue #102: increase version for future release

--- doc/docs/conf.py
+++ doc/docs/conf.py
@@ -31,7 +31,7 @@ author = "Univention GmbH"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "1.8.10"
+release = "1.8.11"
 
 
 # -- General configuration ---------------------------------------------------
