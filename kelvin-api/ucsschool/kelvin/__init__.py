19fcd000929a0e5976a97a9ec927fa9b6a6fe7c9
Ole Schwiegert <schwiegert@univention.de>
2026-03-18 16:11:36 +0100

    build: Make version of ucsschool-kelvin-rest-api dynamic.
    Implement a new function get_version.
    First it will try to read the version from metadata.
    If that fails it will try to read the version from the env variable KELVIN_BUILD_VERSION
    and default to 0.0.0.
    
    Issue univention/dev/education/ucsschool-kelvin-rest-api#151

---
+++ kelvin-api/ucsschool/kelvin/__init__.py
@@ -0,0 +1,13 @@
+def get_version() -> str:
+    import os
+    import re
+    from importlib.metadata import PackageNotFoundError, version
+
+    try:
+        return version("ucsschool-kelvin-rest-api")  # Use the package name from pyproject.toml
+    except PackageNotFoundError:
+        pattern = re.compile(r"\d+\.\d+\.\d+")
+        match = pattern.search(str(os.environ.get("KELVIN_BUILD_VERSION", "0.0.0")))
+        if not match:
+            return "0.0.0"
+        return match.group()
