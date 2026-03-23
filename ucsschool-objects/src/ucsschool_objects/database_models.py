93d7a802c7ebe6ec1beede744e74eed3da06b46f
Jürn Brodersen <brodersen@univention.de>
2026-03-23 16:51:30 +0100

    chore: add some columns for testing
    Issue univention/dev/education/ucsschool-kelvin-rest-api#198

--- ucsschool-objects/src/ucsschool_objects/database_models.py
+++ ucsschool-objects/src/ucsschool_objects/database_models.py
@@ -1,5 +1,17 @@
-from sqlalchemy.orm import DeclarativeBase
+import uuid
+
+from sqlalchemy import UUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class School(Base):
+    __tablename__ = "school"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    public_id: Mapped[uuid.UUID] = mapped_column(
+        UUID, unique=True, index=True, default=uuid.uuid4, nullable=False
+    )
