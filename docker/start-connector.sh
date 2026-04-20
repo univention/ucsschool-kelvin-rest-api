0744f38f6f933299d880c77af5a0a8fe3fd819ec
Johannes Königer <koeniger@univention.de>
2026-04-20 14:02:37 +0200

    feat: add connector skeleton
    Issue univention/dev/education/ucsschool-kelvin-rest-api#220

---
+++ docker/start-connector.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ "$LDAP_SERVER_TYPE" != "master" ]; then
+    echo "Kelvin connector will only run on server type master (LDAP_SERVER_TYPE=\"$LDAP_SERVER_TYPE\")"
+    sleep inf
+else
+    /venv/bin/connector
+fi
