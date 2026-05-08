75320c58bf63c8813c18001fdd6f9d6b8d829dac
Jürn Brodersen <brodersen@univention.de>
2026-05-08 15:33:47 +0200

    fix: wait for provisioning_config.json
    Issue univention/dev/education/ucsschool-kelvin-rest-api#235

--- docker/start-connector.sh
+++ docker/start-connector.sh
@@ -4,6 +4,18 @@ if [ "$LDAP_SERVER_TYPE" != "master" ]; then
     echo "Kelvin connector will only run on server type master (LDAP_SERVER_TYPE=\"$LDAP_SERVER_TYPE\")"
     sleep inf
 else
+    APPCENTER_TIMEOUT=0
+    APP_PROVISIONING_CONFIG_FILE="/var/lib/univention-appcenter/apps/ucsschool-kelvin-rest-api/conf/provisioning_config.json"
+    while [[ ! -f "$APP_PROVISIONING_CONFIG_FILE" ]]; do
+        if [[ APPCENTER_TIMEOUT -gt 60 ]]; then
+            echo "ERROR: joinscript did not write $APP_PROVISIONING_CONFIG_FILE"
+            exit 1
+        fi
+        # Currently the best way to wait for the appcenter to configure the container
+        echo "waiting for joinscript to create $APP_PROVISIONING_CONFIG_FILE"
+        sleep 1
+        ((APPCENTER_TIMEOUT++))
+    done
     echo "Starting connector."
     /venv/bin/connector
 fi
