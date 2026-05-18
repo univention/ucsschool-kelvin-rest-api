53d83bbcb823920924fabdd14a8729b078dbf9a8
Jürn Brodersen <brodersen@univention.de>
2026-05-18 16:07:41 +0200

    chore(SonarQube): Variable names should follow shell naming conventions
    shelldre:S7684
    
    https://sonarqube.knut.univention.de/coding_rules?open=shelldre%3AS7684&rule_key=shelldre%3AS7684
    
    Issue univention/dev/education/ucsschool-kelvin-rest-api#235

--- appcenter/includes/setup-provisioning-subscription.sh
+++ appcenter/includes/setup-provisioning-subscription.sh
@@ -1,74 +1,74 @@
 #!/usr/bin/env bash
 
 setup_provisioning_subscriptions() {
-    local SUBSCRIPTION_NAME="${SUBSCRIPTION_NAME:-kelvin-connector}"
-    local APP_PROVISIONING_CONFIG_FILE="${APP_PROVISIONING_CONFIG_FILE:-/var/lib/univention-appcenter/apps/ucsschool-kelvin-rest-api/conf/provisioning/provisioning_config.json}"
-    local FORCE="${FORCE:-false}"
+    local subscription_name="${SUBSCRIPTION_NAME:-kelvin-connector}"
+    local app_provisioning_config_file="${APP_PROVISIONING_CONFIG_FILE:-/var/lib/univention-appcenter/apps/ucsschool-kelvin-rest-api/conf/provisioning/provisioning_config.json}"
+    local force="${FORCE:-false}"
 
-    mkdir --parent "$(dirname "$APP_PROVISIONING_CONFIG_FILE")"
+    mkdir --parent "$(dirname "$app_provisioning_config_file")"
 
     if [ "$(ucr get "server/role")" != "domaincontroller_master" ]; then
         echo "Not running on primary, skipping provisioning setup."
         return 0
     fi
 
-    if [[ "$FORCE" != "true" ]] && [ -f "$APP_PROVISIONING_CONFIG_FILE" ]; then
-        echo "$APP_PROVISIONING_CONFIG_FILE already exists, skipping provisioning setup."
+    if [[ "$force" != "true" ]] && [ -f "$app_provisioning_config_file" ]; then
+        echo "$app_provisioning_config_file already exists, skipping provisioning setup."
         return 0
     fi
 
-    local FQDN
-    FQDN=$(ucr get ldap/master)
-    local BASE_URL="https://$FQDN/univention/provisioning"
-    local USERNAME="admin"
-    local ADMIN_PASSWORD
-    ADMIN_PASSWORD=$(python3 <<'EOF'
+    local fqdn
+    fqdn=$(ucr get ldap/master)
+    local base_url="https://$fqdn/univention/provisioning"
+    local username="admin"
+    local admin_password
+    admin_password=$(python3 <<'EOF'
 import json
 with open("/etc/provisioning-secrets.json", "r") as f:
     print(json.load(f)["PROVISIONING_API_ADMIN_PASSWORD"])
 EOF
 )
-    local SUBSCRIPTION_PASSWORD
-    SUBSCRIPTION_PASSWORD=$(openssl rand -hex 32)
-    local DATA
+    local subscription_password
+    subscription_password=$(openssl rand -hex 32)
+    local data
     # Order of realms_topics is important, this ensures we get ou's first
-    DATA="$(cat <<EOF
+    data="$(cat <<EOF
 {
-  "name": "$SUBSCRIPTION_NAME",
+  "name": "$subscription_name",
   "realms_topics": [
     {"realm": "udm", "topic": "container/ou"},
     {"realm": "udm", "topic": "users/user"},
     {"realm": "udm", "topic": "groups/group"}
   ],
   "request_prefill": true,
-  "password": "${SUBSCRIPTION_PASSWORD}"
+  "password": "${subscription_password}"
 }
 EOF
 )"
 
     curl --config <(cat <<EOF
-user = "$USERNAME:$ADMIN_PASSWORD"
+user = "$username:$admin_password"
 --silent
 --fail-with-body
 stderr = -
 request = "DELETE"
 header = "Accept: application/json"
 header = "Content-Type: application/json"
-url = "$BASE_URL/v1/subscriptions/$SUBSCRIPTION_NAME"
+url = "$base_url/v1/subscriptions/$subscription_name"
 EOF
 ) || true  # ignore 404 when subscription doesn't exist yet
 
     local response_create
-    response_create=$(curl --data "$DATA" \
+    response_create=$(curl --data "$data" \
         --config <(cat <<EOF
-user = "$USERNAME:$ADMIN_PASSWORD"
+user = "$username:$admin_password"
 --silent
 --fail-with-body
 stderr = -
 request = "POST"
 header = "Accept: application/json"
 header = "Content-Type: application/json"
-url = "$BASE_URL/v1/subscriptions"
+url = "$base_url/v1/subscriptions"
 EOF
 ))
     if [[ $? -ne 0 ]]; then
@@ -76,13 +76,13 @@ EOF
         return 1
     fi
 
-    touch "$APP_PROVISIONING_CONFIG_FILE"
-    chmod 640 "$APP_PROVISIONING_CONFIG_FILE"
+    touch "$app_provisioning_config_file"
+    chmod 640 "$app_provisioning_config_file"
 
-    cat <<EOF > "$APP_PROVISIONING_CONFIG_FILE"
+    cat <<EOF > "$app_provisioning_config_file"
 {
-    "subscription_name": "$SUBSCRIPTION_NAME",
-    "subscription_password": "$SUBSCRIPTION_PASSWORD"
+    "subscription_name": "$subscription_name",
+    "subscription_password": "$subscription_password"
 }
 EOF
 }
