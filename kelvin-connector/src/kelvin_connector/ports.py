e55d8aaba04756195f756fc4b613bad7ba4c2137
Jessica Lai <jessica.lai@univention.de>
2026-07-20 16:15:18 +0200

    chore: Add SPDX header to kelvin-connector files
    Issue univention/dev/education/ucsschool-kelvin-rest-api#216

--- kelvin-connector/src/kelvin_connector/ports.py
+++ kelvin-connector/src/kelvin_connector/ports.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2026 Univention GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
+
 from typing import Callable, Protocol
 
 from ucsschool_objects import DNIDMapper, KelvinStorageSession, KelvinStorageSessionFactory
